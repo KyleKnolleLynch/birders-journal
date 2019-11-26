4887e823bf54af872c22450a0adea74711c08397
Kyle Lynch <kylelynch104@gmail.com>
2019-11-26 17:40:31 -0600

    dynamic cache, cache limit, not found page

--- sw.js
+++ sw.js
@@ -1,38 +1,51 @@
-const staticCacheName = "site-static-v1";
+const staticCache = 'site-static-v1';
+const dynamicCache = 'site-dynamic-v1';
 const assets = [
-  "/",
-  "/index.html",
-  "/js/app.js",
-  "/js/ui.js",
-  "/js/materialize.min.js",
-  "/css/style.css",
-  "/css/materialize.min.css",
-  "/img/logo.jpg",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
-  "https://fonts.gstatic.com/s/calistoga/v1/6NUU8F2OJg6MeR7l4e0fvMwB8dQ.woff2",
-  "https://fonts.gstatic.com/s/calistoga/v1/6NUU8F2OJg6MeR7l4e0fs8wB.woff2"
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/style.css',
+  '/css/materialize.min.css',
+  '/img/logo.jpg',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+  'https://fonts.gstatic.com/s/calistoga/v1/6NUU8F2OJg6MeR7l4e0fvMwB8dQ.woff2',
+  'https://fonts.gstatic.com/s/calistoga/v1/6NUU8F2OJg6MeR7l4e0fs8wB.woff2',
+  '/pages/notFound.html'
 ];
 
+//  limit cache size
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
 //  install service worker
-self.addEventListener("install", evt => {
+self.addEventListener('install', evt => {
   // console.log("service worker has been installed!");
   evt.waitUntil(
-    caches.open(staticCacheName).then(cache => {
-      console.log("caching shell assets");
+    caches.open(staticCache).then(cache => {
+      // console.log('caching shell assets');
       cache.addAll(assets);
     })
   );
 });
 
 //  activate event
-self.addEventListener("activate", evt => {
+self.addEventListener('activate', evt => {
   // console.log("service worker has been activated");
   evt.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
         keys
-          .filter(key => key !== staticCacheName)
+          .filter(key => key !== staticCache && key !== dynamicCache)
           .map(key => caches.delete(key))
       );
     })
@@ -40,11 +53,26 @@ self.addEventListener("activate", evt => {
 });
 
 //  fetch event
-self.addEventListener("fetch", evt => {
-  // console.log("fetch event", evt);
+self.addEventListener('fetch', evt => {
   evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
+    caches
+      .match(evt.request)
+      .then(cacheRes => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then(fetchRes => {
+            return caches.open(dynamicCache).then(cache => {
+              cache.put(evt.request.url, fetchRes.clone());
+              limitCacheSize(dynamicCache, 20);
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (evt.request.url.includes('.html')) {
+          return caches.match('/pages/notFound.html');
+        }
+      })
   );
 });
